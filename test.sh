d2eecebdfff9b7ffedc183f938840dd946641b74
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-07-22 21:22:32 +0900

    test.sh改良

--- test.sh
+++ test.sh
@@ -3,8 +3,13 @@ assert() {
   expected="$1"
   input="$2"
 
+  echo "******************************** [information] ********************************"
+  echo "Input:$input"
+  echo "********************************** [compile] **********************************"
   ./9cc "$input" > tmp.s
+  echo "********************************* [assemble] **********************************"
   cc -o tmp tmp.s
+  echo "********************************* [execution] *********************************"
   ./tmp
   actual="$?"
 
@@ -12,8 +17,11 @@ assert() {
     echo "$input => $actual"
   else
     echo "$input => $expected expected, but got $actual"
-    exit 1
+    # exit 1
   fi
+  
+  echo ""
+  echo ""
 }
 
 # assert 理想の実行結果 入力データ
@@ -27,5 +35,6 @@ assert() {
 # y = tmp;
 # "
 assert "14" "a = 3; b = 5 * 6 - 8; return a + b / 2;"
+assert "10" "i=0; while(i<10) i++; return i;"
 
-echo OK
+echo finished
