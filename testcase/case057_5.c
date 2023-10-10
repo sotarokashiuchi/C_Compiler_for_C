539947d5b3832c76f4f4d66ba4961208f154da5a
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-10-10 19:27:11 +0900

    add testcase

---
+++ testcase/case057_5.c
@@ -0,0 +1,11 @@
+int main(){
+  int x = 5;
+	int y = 3;
+	if(x==y || x==3 || y==3){
+		if(x==y || x==5 && y==3){
+			return x;
+		}
+	}
+	return 1;
+}
+
---
+++ testcase/case058_5.c
@@ -0,0 +1,8 @@
+int main(){
+	int x;;;;
+	x = 5;
+	if(x==5){
+		return x;;;
+	}
+}
+
